41146da570921ed6e69784520085598c3ad23398
Siddastiy <andrejmatasov969@gmail.com>
2025-08-30 22:46:22 +0500

    added some features

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function changeTheme() {
     document.body.classList.toggle("dark-theme");
 }
 
+function showText() {
+    document.getElementById("popup").classList.toggle("show-text");
+}
+
 function playSound() {
     let sound = new Audio("sound.mp3");
     sound.play()
